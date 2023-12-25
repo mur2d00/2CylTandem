ed19992ffe219db197e1f330066b83c71e38be59
MuradNazari <38508868+mur2d00@users.noreply.github.com>
2023-12-24 23:25:17 -0600

    Update pview.py

--- pview.py
+++ pview.py
@@ -1,13 +1,8 @@
 # A paraview script to load openfoam data and save the output data as csv at a plane.
 from paraview.simple import *
 
-# Load OpenFOAM data
 viewfoam = OpenFOAMReader(FileName='view.foam')
-
-# Update to ensure data is loaded
 viewfoam.UpdatePipeline()
-
-# Create a clip & a slice filter
 clipFilter = Clip(registrationName='Clip1', Input=viewfoam)
 clipFilter.ClipType = 'Box'
 clipFilter.ClipType.Position = [-0.6, -0.3, -0.1]
@@ -16,19 +11,11 @@ sliceFilter = Slice(registrationName='sliceFilter', Input=clipFilter)
 sliceFilter.SliceType = 'Plane'
 sliceFilter.SliceType.Origin = [0.0, 0.0, 0.0]
 sliceFilter.SliceType.Normal = [0.0, 1.0, 0.0]
-# Get the animation scene
 animationScene = GetAnimationScene()
 
-# Loop through each time step
 for index, timeStep in enumerate(animationScene.TimeKeeper.TimestepValues):
     # Update the time step
     animationScene.AnimationTime = timeStep
-    
-    # Update the pipeline for the new time step
     UpdatePipeline(timeStep, sliceFilter)
-
-    # Define the output filename for each time step
     filename = f'sliced_data_timestep_{index}.csv'
-
-    # Export the sliced data to a CSV file
     SaveData(filename, proxy=sliceFilter)
