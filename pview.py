b2012b9f4ce6514a2af2fdc6da19e25a655cf3ee
MuradNazari <38508868+mur2d00@users.noreply.github.com>
2023-12-30 01:09:51 -0600

    Update pview.py

--- pview.py
+++ pview.py
@@ -1,22 +1,49 @@
-# A paraview script to load openfoam data and save the output data as csv at a plane.
-# Run this script by creating a view.foam file in OpenFOAM case directory and run paraview by "pvpython pview.py". The script will output all quantities in CSV file.
+# A paraview script to import openfoam data and save dataPoints at probed planes
+import paraview.simple as pvs
+import numpy as np
 
-from paraview.simple import *
-
-viewfoam = OpenFOAMReader(FileName='view.foam')
+# Load OpenFOAM data
+viewfoam = pvs.OpenFOAMReader(FileName='view.foam')
 viewfoam.UpdatePipeline()
-clipFilter = Clip(registrationName='Clip1', Input=viewfoam)
+calculator = pvs.Calculator(Input=viewfoam)
+# calculate velocity of sediment and solid phase
+calculator.ResultArrayName = 'Ua'
+calculator.Function = '"alpha.a"*"U.a"'
+calculator.UpdatePipeline()
+calculator2 = pvs.Calculator(Input=calculator)
+calculator2.ResultArrayName = 'Ub'
+calculator2.Function = '"alpha.b"*"U.b"'
+calculator2.UpdatePipeline()
+# calculate the gradient of velocity vectors
+gradient1 = pvs.Gradient(Input=calculator2)
+gradient1.ScalarArray = ['POINTS', 'Ua',]
+gradient1.UpdatePipeline()
+gradient2 = pvs.Gradient(Input=gradient1)
+gradient2.ScalarArray = ['POINTS', 'Ub']
+gradient2.UpdatePipeline()
+# calculate s of sediment phase
+gradS1 = pvs.inputs[0].PointData['gradient1']
+S_sediment = (gradS1 + np.transpose(gradS1)) / 2
+pvs.output.PointData.append(S_sediment, 'Ss')
+# calculate S of fluid phase
+gradS2 = pvs.inputs[0].PointData['gradient2']
+S_fluid = (gradS2 + np.transpose(gradS2)) / 2
+pvs.output.PointData.append(S_fluid, 'Sf')
+# create a box around 2 cylinders
+clipFilter = pvs.Clip(registrationName='Clip1', Input=gradient2)
 clipFilter.ClipType = 'Box'
 clipFilter.ClipType.Position = [-0.6, -0.3, -0.1]
 clipFilter.ClipType.Length = [1.2, 0.6, 0.25]
-sliceFilter = Slice(registrationName='sliceFilter', Input=clipFilter)
+# create a slice normal to y-plane
+sliceFilter = pvs.Slice(registrationName='sliceFilter', Input=clipFilter)
 sliceFilter.SliceType = 'Plane'
 sliceFilter.SliceType.Origin = [0.0, 0.0, 0.0]
 sliceFilter.SliceType.Normal = [0.0, 1.0, 0.0]
-animationScene = GetAnimationScene()
+animationScene = pvs.GetAnimationScene()
 
+# Loop through each time step
 for index, timeStep in enumerate(animationScene.TimeKeeper.TimestepValues):
     animationScene.AnimationTime = timeStep
-    UpdatePipeline(timeStep, sliceFilter)
+    pvs.UpdatePipeline(timeStep, sliceFilter)
     filename = f'sliced_data_timestep_{index}.csv'
-    SaveData(filename, proxy=sliceFilter)
+    pvs.SaveData(filename, proxy=sliceFilter)
